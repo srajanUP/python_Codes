74e92c421aa9cccf17a59ba8cf59f232e2735dbc
srajanUP <srajanupadhyay251@gmail.com>
2024-01-09 17:16:06 +0530

    adding list and string

---
+++ List.py
@@ -0,0 +1,32 @@
+
+# immutable string
+name="pyhton"
+print(name[-1])
+print(len(name))
+
+
+
+
+# traversing the string
+name1="srajan upadhyay"
+for i in name1:
+    print(i,end="~")
+
+
+# concatination in string
+print("\nhello "+"world")   #the operands on the both sides of the + operator must same
+
+#string replication
+print("srajan"*3)          #both goperands on both sides of * operator can not be string  either numbers or number and strin
+
+#comparision operators
+print("a"=="A")
+print("a"!="A")
+print("a"<"A")
+
+# to get the ASCII number of the character
+print(ord("a"))
+
+# to get the ASCII cahracter from the number
+print(chr(97))
+
---
+++ string.py
@@ -0,0 +1,12 @@
+
+
+name="srajan"
+# name[0]="k"  can not change the value as string are immutable in python
+print(name)
+
+#print the name from 0 to 5-1 index
+print(name[0:5])
+
+print(len(name))
+
+name.length()
